bb4f2c660a92b650b8eb12b143ca61b846c23147
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2023-07-20 12:17:55 +1000

    Change messages in example

--- examples/button_test.py
+++ examples/button_test.py
@@ -2,17 +2,17 @@ from haiku import BApplication, BWindow, BRect, BMessage, BView, BButton, window
  
 class Window(BWindow):
     def __init__(self):
-        BWindow.__init__(self, BRect(100,100,400,400), "Hola PyBind11", window_type.B_TITLED_WINDOW, 0)
+        BWindow.__init__(self, BRect(100,100,400,400), "Hello Haiku", window_type.B_TITLED_WINDOW, 0)
         self.say_hi = BMessage(1)
         self.panel = BView(self.Bounds(), "panel", 8, 20000000)
-        self.button = BButton(BRect(10,10,50,50), "hi", "Hola", self.say_hi)
+        self.button = BButton(BRect(10,10,50,50), "hi", "Say hi", self.say_hi)
         self.panel.AddChild(self.button, None)
         self.AddChild(self.panel, None)
         self.Show()
  
     def MessageReceived(self, msg):
         if msg.what == self.say_hi.what:
-            print("SAY hi!")
+            print("hi!")
         else:
             BWindow.MessageReceived(self, msg)
  
