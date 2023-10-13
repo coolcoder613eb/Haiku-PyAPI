0568b00adda22037b89862bf1189dc94453ea614
Zardshard <0azrune6@zard.anonaddy.com>
2023-10-13 07:43:10 -0400

    Cleanup old files

--- build.sh
+++
@@ -1,7 +0,0 @@
-cd bindings
-echo Compiling bindings...
-g++ -shared -std=c++11 -fPIC -I/system/lib/python3.9/vendor-packages/pybind11/include/ -I/system/develop/headers/python3.9/ haiku.cpp -lbe -o _haiku.so
-echo Finishing up...
-cp _haiku.so ../api
-mv _haiku.so ../bin
-cd ..
