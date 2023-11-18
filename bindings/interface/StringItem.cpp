2a4b5ba14b6788631e12694accacd73422ccc8bb
Fabio Tomat <f.t.public@gmail.com>
2023-11-18 21:17:38 +0100

    missing elements GraphicsDefs.cpp, revert StringItem no derivated classes allowed

--- bindings/interface/StringItem.cpp
+++ bindings/interface/StringItem.cpp
@@ -9,7 +9,7 @@
 #include <View.h>
 
 namespace py = pybind11;
-
+/*
 class PyStringItem : public BStringItem{
 	public:
         using BStringItem::BStringItem;
@@ -17,11 +17,13 @@ class PyStringItem : public BStringItem{
             PYBIND11_OVERLOAD(void, BStringItem, DrawItem, owner, frame, complete);
         }
         // TODO: there may be more methods that we should allow overloading
+private:
+    BStringItem* item_;
 };
-
+*/
 PYBIND11_MODULE(StringItem,m)
 {
-py::class_<BStringItem, PyStringItem, BListItem>(m, "BStringItem")
+py::class_<BStringItem, BListItem,std::unique_ptr<BStringItem, py::nodelete>>(m, "BStringItem")
 .def(py::init<const char *, unsigned int, bool>(), "", py::arg("text"), py::arg("outlineLevel")=0, py::arg("expanded")=true)
 .def(py::init<BMessage *>(), "", py::arg("archive"))
 .def_static("Instantiate", &BStringItem::Instantiate, "", py::arg("archive"))
