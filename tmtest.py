f8d071363e5918427f0dea3212b218ce4bc4153f
Fabio Tomat <f.t.public@gmail.com>
2023-11-18 19:02:16 +0100

    enable GraphicDefs and parts of View

--- tmtest.py
+++ tmtest.py
@@ -1,6 +1,6 @@
-from Be import BApplication, BWindow, BListItem, BPicture, BStringItem, BAlert, BPoint, BBox, BListView, BScrollView, BRadioButton, BColorControl, BCheckBox, BRect, BTextControl, BView,BMenu,BStatusBar, BMenuBar, BMenuItem,BSeparatorItem,BStringView,BMessage,window_type,  B_NOT_RESIZABLE, B_QUIT_ON_WINDOW_CLOSE
+from Be import BApplication, BWindow, BListItem, BTabView, BTab, BFont, BPicture, BStringItem, BAlert, BPoint, BBox, BListView, BScrollView, BRadioButton, BColorControl, BCheckBox, BRect, BTextControl, BView,BMenu,BStatusBar, BMenuBar, BMenuItem,BSeparatorItem,BStringView,BMessage,window_type,  B_NOT_RESIZABLE, B_QUIT_ON_WINDOW_CLOSE
 #from Be import BListItem #BStringItem
-from Be import BTabView, BTab, BFont
+from Be.GraphicsDefs import *#rgb_color
 from Be.ListView import list_view_type
 from Be.Alert import alert_type
 from Be.ColorControl import color_control_layout
@@ -10,21 +10,28 @@ from Be import AppDefs
 
 class StrangeItem(BStringItem):
 	nocolor = (0, 0, 0, 0)
+	gcolor = (0, 200, 0, 0)
 	def __init__(self, testo,colore):
 		self.testo = testo
 		self.color = colore
 		BStringItem.__init__(self,testo)
+	def Text(self):
+		return self.testo
+	def Color(self):
+		return self.color
 	def DrawItem(self,owner,frame,complete):
+		print("dissegno StrangeItem")
 		if self.IsSelected() or complete:
 			color = (50,50,50,255)
-			owner.SetHighColor(color)
-			owner.SetLowColor(color)
-			owner.FillRect(frame)
+			owner.SetHighColor(50,50,50,255)
+			owner.SetLowColor(50,50,50,255)
+			#owner.FillRect(frame)
 			self.color=self.nocolor
-		owner.SetHighColor(self.color)
-		owner.MovePenTo(0,frame.Height()-2)
-		owner.DrawString(self.testo)
-		owner.SetLowColor((255,255,255,255))
+		owner.SetHighColor(0, 200, 0, 0)
+		#owner.MovePenTo(0,frame.Height()-2)
+		#owner.DrawString(self.testo)
+		owner.SetLowColor(255,255,255,255)
+		BStringItem.DrawItem(self,owner,frame,complete)
 
 class NewsItem(BListItem):
 	nocolor = (0, 0, 0, 0)
@@ -37,21 +44,26 @@ class NewsItem(BListItem):
 	def DrawItem(self, owner, frame, complete):
 		if self.IsSelected() or complete:
 			color = (200,200,200,255)
-			#owner.SetHighColor(color)
-			#owner.SetLowColor(color)
+			owner.SetHighColor(200,200,200,255)
+			owner.SetLowColor(200,200,200,255)
+			paternale=Pattern()
+			paternale.data=[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+			#paternale = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+			BView.FillRect(frame,paternale.toPattern())
 			#owner.FillRect(frame)
 			self.color=self.nocolor
-		#owner.SetHighColor(self.color)
-		#if self.color == (200,0,0,0):
-		#	self.font = be_bold_font
-		#	owner.SetFont(self.font)
-		#else:	
-		#	self.font = be_plain_font
-		#	owner.SetFont(self.font)
+#		#owner.SetHighColor(self.color)
+#		#if self.color == (200,0,0,0):
+#		#	self.font = be_bold_font
+#		#	owner.SetFont(self.font)
+#		#else:	
+#		#	self.font = be_plain_font
+#		#	owner.SetFont(self.font)
 		frame.PrintToStream()
-		#owner.MovePenTo(frame[0],frame[3]-2)
-		#owner.DrawString(self.name)
-		#owner.SetLowColor((255,255,255,255))
+		owner.MovePenTo(0,frame.Height()-2)
+		owner.DrawString("->"+self.name,None)
+#		#owner.SetLowColor((255,255,255,255))
+		#BListItem.DrawItem(self,owner,frame,complete)
 
 	def Text(self):
 		return self.name
@@ -84,6 +96,10 @@ class Window(BWindow):
 		print(bounds.LeftTop())
 		self.panel = BView(self.Bounds(), "panel", 8, 20000000)
 		self.box = BBox(BRect(10,26,90,51),"MYBox",0x0202|0x0404,InterfaceDefs.border_style.B_FANCY_BORDER)
+		#self.box.SetHighColor(0, 200, 0, 0)
+		#paternale=Pattern()
+		#paternale.data=[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+		#self.box.FillRect(self.box.Bounds(),paternale.toPattern())
 		self.bar = BMenuBar(self.box.Bounds(), 'Bar')
 		self.testo = BStringView(BRect(5,5,75,20), 'Test','prova')
 		self.box.AddChild(self.testo,None)
@@ -98,6 +114,12 @@ class Window(BWindow):
 		self.sevenradio = BRadioButton(BRect(8,236,24,252),'tepidradio', 'tepid', BMessage(7))
 		self.nineradio = BRadioButton(BRect(8,252,24,268),'coolradio', 'cool', BMessage(9))
 		self.list = ScrollView(BRect(18 , 300, bounds.Width() - 18 , bounds.Height() - 40 ), 'ScrollView')
+		colore=self.list.lv.HighColor()
+		print("colore è:",colore.red,colore.green,colore.blue,colore.alpha)
+		colore.set_to(10,200,100,255)
+		print("colore modificato è:",colore.red,colore.green,colore.blue,colore.alpha)
+		self.list.lv.SetHighColor(colore)
+		self.list.lv.SetLowColor(colore)
 		lollo=BPicture()
 		self.list.sv.Hide()
 		global newsitem
@@ -109,16 +131,19 @@ class Window(BWindow):
 			# reference to NewsItem and so cannot free it until Haiku-PyAPI
 			# is finished with it
 		newsitem = NewsItem("Nuova",(255,0,0,0))
-		strano = StrangeItem("Prova",(0,200,0,0))
+		self.elementolista=BStringItem("Questo è un BStringItem")
+		#global strano
+		strano = StrangeItem("Questo è un StrangeItem",(0,200,0,0))
 		print(type(self.list.lv))
 		print(self.list.lv.Items())
 		print(self.list.lv.CountItems())
 		self.list.lv.AddItem(newsitem)
+		self.list.lv.AddItem(self.elementolista)
 		self.list.lv.AddItem(strano)
 		print(self.list.lv.Items())
-		print(self.list.lv.IndexOf(newsitem))
+		print(self.list.lv.IndexOf(self.elementolista))
 		self.list.sv.Show()
-		self.panel.AddChild(self.list.topview(),None)
+		#self.panel.AddChild(self.list.topview(),None)
 		self.panel.AddChild(self.sixradio,None)
 		self.panel.AddChild(self.sevenradio,None)
 		self.panel.AddChild(self.nineradio,None)
@@ -127,6 +152,14 @@ class Window(BWindow):
 		self.panel.AddChild(self.tachetest,None)
 		self.panel.AddChild(self.statbar,None)
 		self.panel.AddChild(self.bar,None)
+		for menu, items in self.Menus:
+			menu = BMenu(menu)
+			for k, name in items:
+				if k is None:
+						menu.AddItem(BSeparatorItem())
+				else:
+						menu.AddItem(BMenuItem(name, BMessage(k),name[1],0))
+			self.bar.AddItem(menu)
 		self.tabslabels=[]
 		self.tabsviews=[]
 		self.tabsviews.append(self.panel)
@@ -137,24 +170,15 @@ class Window(BWindow):
 		#BFont.GetStringWidths(testotab,len(testotab),1,outarray)#StringWidth(chr(ord("P")))
 		#print(outarray)
 		tabrect=BRect(3.0, 3.0, 30.0, self.maintabview.TabHeight()-3.0)
-		bviewtab=BStringView(tabrect,"scheda1","Princpiale")
-		scheda=BTab(bviewtab)#self.maintabview)
+		#bviewtab=BStringView(tabrect,"scheda1","Princpiale")
+		scheda=BTab(None)#bviewtab)#self.maintabview)
 		scheda.SetLabel("Principale")
 		self.tabslabels.append(scheda)
-		
-		print(self.tabslabels[0])
-		print(self.tabsviews[0])
-		self.maintabview.AddTab(self.panel,scheda)#self.tabsviews[0],self.tabslabels[0])
+		self.maintabview.AddTab(self.tabsviews[0],scheda)#self.tabsviews[0],self.tabslabels[0])
 		self.bckgnd.AddChild(self.maintabview,None)
-		for menu, items in self.Menus:
-			menu = BMenu(menu)
-			for k, name in items:
-				if k is None:
-						menu.AddItem(BSeparatorItem())
-				else:
-						menu.AddItem(BMenuItem(name, BMessage(k),name[1],0))
-			self.bar.AddItem(menu)
+		scheda.SetLabel("Principale")
 		self.AddChild(self.bckgnd,None)
+		self.panel.AddChild(self.list.topview(),None)
 		
 	def MessageReceived(self, msg):
 		if msg.what == 1:
