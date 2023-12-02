7db8799303fa9b17abd51d3a7f5871b2af8a8c65
Fabio Tomat <f.t.public@gmail.com>
2023-12-02 13:29:24 +0100

    Add CheckBox.cpp overrides

--- bindings/interface/CheckBox.cpp
+++ bindings/interface/CheckBox.cpp
@@ -10,6 +10,92 @@
 namespace py = pybind11;
 
 
+class PyBCheckBox : public BCheckBox{
+	public:
+        using BCheckBox::BCheckBox;
+        status_t	Archive(BMessage* data, bool deep = true) const override {
+        	PYBIND11_OVERLOAD(status_t, BCheckBox, Archive, data, deep);
+        }
+        void				Draw(BRect updateRect) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, Draw, updateRect);
+        }
+        void				AttachedToWindow() override {
+            PYBIND11_OVERLOAD(void, BCheckBox, AttachedToWindow);
+        }
+        void				DetachedFromWindow() override {
+            PYBIND11_OVERLOAD(void, BCheckBox, DetachedFromWindow);
+        }
+        void				AllAttached() override {
+            PYBIND11_OVERLOAD(void, BCheckBox, AllAttached);
+        }
+        void				AllDetached() override {
+            PYBIND11_OVERLOAD(void, BCheckBox, AllDetached);
+        }
+        void				FrameMoved(BPoint newPosition) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, FrameMoved, newPosition);
+        }
+        void				FrameResized(float newWidth, float newHeight) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, FrameResized, newWidth, newHeight);
+        }
+        void				WindowActivated(bool active) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, WindowActivated, active);
+        }
+        void				MessageReceived(BMessage* message) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, MessageReceived, message);
+        }
+        void				KeyDown(const char* bytes, int32 numBytes) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, KeyDown, bytes, numBytes);
+        }
+        void				MouseDown(BPoint where) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, MouseDown, where);
+        }
+        void				MouseUp(BPoint where) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, MouseUp, where);
+        }
+        void				MouseMoved(BPoint where, uint32 code, const BMessage* dragMessage) override {
+        	PYBIND11_OVERLOAD(void, BCheckBox, MouseMoved, where, code, dragMessage);
+        }
+        void				GetPreferredSize(float* _width, float* _height) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, GetPreferredSize, _width, _height);
+        }
+        void				ResizeToPreferred() override {
+            PYBIND11_OVERLOAD(void, BCheckBox, ResizeToPreferred);
+        }
+        BSize				MinSize() override {
+            PYBIND11_OVERLOAD(BSize, BCheckBox, MinSize);
+        }
+        BSize				MaxSize() override {
+            PYBIND11_OVERLOAD(BSize, BCheckBox, MaxSize);
+        }
+        BSize				PreferredSize() override {
+            PYBIND11_OVERLOAD(BSize, BCheckBox, PreferredSize);
+        }
+        BAlignment			LayoutAlignment() override {
+            PYBIND11_OVERLOAD(BAlignment, BCheckBox, LayoutAlignment);
+        }
+        void				MakeFocus(bool focused = true) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, MakeFocus, focused);
+        }
+        void				SetValue(int32 value) override {
+            PYBIND11_OVERLOAD(void, BCheckBox, SetValue, value);
+        }
+        status_t			Invoke(BMessage* message = NULL) override {
+            PYBIND11_OVERLOAD(status_t, BCheckBox, Invoke, message);
+        }
+        BHandler*			ResolveSpecifier(BMessage* message, int32 index, BMessage* specifier, int32 what, const char* property) override {
+            PYBIND11_OVERLOAD(BHandler*, BCheckBox, ResolveSpecifier, message, index, specifier, what, property);
+        }
+        status_t			GetSupportedSuites(BMessage* message) override {
+            PYBIND11_OVERLOAD(status_t, BCheckBox, GetSupportedSuites, message);
+        }
+        status_t			Perform(perform_code code, void* data) override {
+            PYBIND11_OVERLOAD(status_t, BCheckBox, Perform, code, data);
+        }
+        status_t			SetIcon(const BBitmap* icon, uint32 flags = 0) override {
+            PYBIND11_OVERLOAD(status_t, BCheckBox, SetIcon, icon, flags);
+        }
+};
+
 PYBIND11_MODULE(CheckBox,m)
 {
 py::class_<BCheckBox, BControl, std::unique_ptr<BCheckBox, py::nodelete>>(m, "BCheckBox")
