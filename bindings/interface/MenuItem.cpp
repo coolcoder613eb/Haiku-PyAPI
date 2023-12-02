09748eac5ab50bbee8a0c74886344cb886938b75
Fabio Tomat <f.t.public@gmail.com>
2023-12-02 18:21:36 +0100

    Add MenuItem.cpp overrides

--- bindings/interface/MenuItem.cpp
+++ bindings/interface/MenuItem.cpp
@@ -13,11 +13,34 @@
 namespace py = pybind11;
 using namespace BPrivate;
 
+class PyBMenuItem : public BMenuItem{
+	public:
+        using BMenuItem::BMenuItem;
+        status_t	Archive(BMessage* archive, bool deep = true) const override {
+        	PYBIND11_OVERLOAD(status_t, BMenuItem, Archive, archive, deep);
+        }
+        void		SetLabel(const char* name) override {
+        	PYBIND11_OVERLOAD(void, BMenuItem, SetLabel, name);
+        }
+        void		SetEnabled(bool enable) override {
+        	PYBIND11_OVERLOAD(void, BMenuItem, SetEnabled, enable);
+        }
+        void		SetMarked(bool mark) override {
+        	PYBIND11_OVERLOAD(void, BMenuItem, SetMarked, mark);
+        }
+        void		SetTrigger(char trigger) override {
+        	PYBIND11_OVERLOAD(void, BMenuItem, SetTrigger, trigger);
+        }
+        void		SetShortcut(char shortcut, uint32 modifiers) override {
+        	PYBIND11_OVERLOAD(void, BMenuItem, SetShortcut, shortcut, modifiers);
+        }
+};
+
 PYBIND11_MODULE(MenuItem,m)
 {
 //m.attr("MenuItemPrivate") = MenuItemPrivate;
 
-py::class_<BMenuItem, BInvoker, std::unique_ptr<BMenuItem, py::nodelete>>(m, "BMenuItem")
+py::class_<BMenuItem, PyBMenuItem, BInvoker, std::unique_ptr<BMenuItem, py::nodelete>>(m, "BMenuItem")
 .def(py::init<const char *, BMessage *, char, unsigned int>(), "", py::arg("label"), py::arg("message"), py::arg("shortcut")=0, py::arg("modifiers")=0)
 .def(py::init<BMenu *, BMessage *>(), "", py::arg("menu"), py::arg("message")=NULL)
 .def(py::init<BMessage *>(), "", py::arg("data"))
@@ -37,6 +60,5 @@ py::class_<BMenuItem, BInvoker, std::unique_ptr<BMenuItem, py::nodelete>>(m, "BM
 .def("Menu", &BMenuItem::Menu, "")
 .def("Frame", &BMenuItem::Frame, "")
 ;
-
 
 }
