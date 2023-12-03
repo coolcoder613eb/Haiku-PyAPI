96e0cddabae7ddf807d450405d649530f4342a76
Fabio Tomat <f.t.public@gmail.com>
2023-12-03 16:04:09 +0100

    Add SeparatorItem.cpp overrides

--- bindings/interface/SeparatorItem.cpp
+++ bindings/interface/SeparatorItem.cpp
@@ -8,10 +8,20 @@
 
 namespace py = pybind11;
 
+class PyBSeparatorItem : public BSeparatorItem{
+	public:
+        using BSeparatorItem::BSeparatorItem;
+        status_t		Archive(BMessage* data, bool deep = true) const override {
+        	PYBIND11_OVERLOAD(status_t, BSeparatorItem, Archive, data, deep);
+        }
+        void			SetEnabled(bool enable) override {
+        	PYBIND11_OVERLOAD(void, BSeparatorItem, SetEnabled, enable);
+        }
+};
 
 PYBIND11_MODULE(SeparatorItem,m)
 {
-py::class_<BSeparatorItem, BMenuItem, std::unique_ptr<BSeparatorItem, py::nodelete>>(m, "BSeparatorItem")
+py::class_<BSeparatorItem, PyBSeparatorItem, BMenuItem, std::unique_ptr<BSeparatorItem, py::nodelete>>(m, "BSeparatorItem")
 .def(py::init(), "")
 .def(py::init<BMessage *>(), "", py::arg("data"))
 .def_static("Instantiate", &BSeparatorItem::Instantiate, "", py::arg("data"))
