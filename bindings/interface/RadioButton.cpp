e72e01c001fc956f1ff85a6f119599b4e2ea8bee
Fabio Tomat <f.t.public@gmail.com>
2023-12-03 08:12:05 +0100

    Add RadioButton.cpp overrides

--- bindings/interface/RadioButton.cpp
+++ bindings/interface/RadioButton.cpp
@@ -9,10 +9,89 @@
 
 namespace py = pybind11;
 
+class PyBRadioButton : public BRadioButton{
+	public:
+        using BRadioButton::BRadioButton;
+        status_t		Archive(BMessage* data, bool deep = true) const override {
+        	PYBIND11_OVERLOAD(status_t, BRadioButton, Archive, data, deep);
+        }
+        void			Draw(BRect updateRect) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, Draw, updateRect);
+        }
+        void			MouseDown(BPoint where) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, MouseDown, where);
+        }
+        void			AttachedToWindow() override {
+            PYBIND11_OVERLOAD(void, BRadioButton, AttachedToWindow);
+        }
+        void			KeyDown(const char* bytes, int32 numBytes) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, KeyDown, bytes, numBytes);
+        }
+        void			SetValue(int32 value) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, SetValue, value);
+        }
+        void			GetPreferredSize(float* _width, float* _height) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, GetPreferredSize, _width, _height);
+        }
+        void			ResizeToPreferred() override {
+            PYBIND11_OVERLOAD(void, BRadioButton, ResizeToPreferred);
+        }
+        status_t		Invoke(BMessage* message = NULL) override {
+            PYBIND11_OVERLOAD(status_t, BRadioButton, Invoke, message);
+        }
+        void			MessageReceived(BMessage* message) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, MessageReceived, message);
+        }
+        void			WindowActivated(bool active) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, WindowActivated, active);
+        }
+        void			MouseUp(BPoint where) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, MouseUp, where);
+        }
+        void			MouseMoved(BPoint where, uint32 code, const BMessage* dragMessage) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, MouseMoved, where, code, dragMessage);
+        }
+        void			DetachedFromWindow() override {
+            PYBIND11_OVERLOAD(void, BRadioButton, DetachedFromWindow);
+        }
+        void			FrameMoved(BPoint newPosition) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, FrameMoved, newPosition);
+        }
+        void			FrameResized(float newWidth, float newHeight) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, FrameResized, newWidth, newHeight);
+        }
+        BHandler*		ResolveSpecifier(BMessage* message, int32 index, BMessage* specifier, int32 what, const char* property) override {
+            PYBIND11_OVERLOAD(BHandler*, BRadioButton, ResolveSpecifier, message, index, specifier, what, property);
+        }
+        void			MakeFocus(bool focus = true) override {
+            PYBIND11_OVERLOAD(void, BRadioButton, MakeFocus, focus);
+        }
+        void			AllAttached() override {
+            PYBIND11_OVERLOAD(void, BRadioButton, AllAttached);
+        }
+        void			AllDetached() override {
+            PYBIND11_OVERLOAD(void, BRadioButton, AllDetached);
+        }
+        status_t		GetSupportedSuites(BMessage* message) override {
+            PYBIND11_OVERLOAD(status_t, BRadioButton, GetSupportedSuites, message);
+        }
+        status_t		Perform(perform_code d, void* argument) override {
+            PYBIND11_OVERLOAD(status_t, BRadioButton, Perform, d, argument);
+        }
+        BSize			MaxSize() override {
+            PYBIND11_OVERLOAD(BSize, BRadioButton, MaxSize);
+        }
+        BAlignment		LayoutAlignment() override {
+            PYBIND11_OVERLOAD(BAlignment, BRadioButton, LayoutAlignment);
+        }
+        status_t		SetIcon(const BBitmap* icon, uint32 flags = 0) override {
+            PYBIND11_OVERLOAD(status_t, BRadioButton, SetIcon, icon, flags);
+        }
+};
 
 PYBIND11_MODULE(RadioButton,m)
 {
-py::class_<BRadioButton, BControl,std::unique_ptr<BRadioButton, py::nodelete>>(m, "BRadioButton")
+py::class_<BRadioButton, PyBRadioButton, BControl,std::unique_ptr<BRadioButton, py::nodelete>>(m, "BRadioButton")
 .def(py::init<BRect, const char *, const char *, BMessage *, unsigned int, unsigned int>(), "", py::arg("frame"), py::arg("name"), py::arg("label"), py::arg("message"), py::arg("resizingMode")=B_FOLLOW_LEFT_TOP, py::arg("flags")=B_WILL_DRAW | B_NAVIGABLE)
 .def(py::init<const char *, const char *, BMessage *, unsigned int>(), "", py::arg("name"), py::arg("label"), py::arg("message"), py::arg("flags")=B_WILL_DRAW | B_NAVIGABLE)
 .def(py::init<const char *, BMessage *>(), "", py::arg("label"), py::arg("message"))
