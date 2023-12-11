d8bbcf37da41f134953ad18a12ead0227bdd1230
Fabio Tomat <f.t.public@gmail.com>
2023-12-11 21:30:08 +0100

    inital work on AppFileInfo.cpp

--- bindings/storage/AppFileInfo.cpp
+++ bindings/storage/AppFileInfo.cpp
@@ -4,11 +4,21 @@
 #include <pybind11/operators.h>
 
 #include <AppFileInfo.h>
+#include <Bitmap.h>
 
 namespace py = pybind11;
 
+struct version_info {
+    uint32_t major;
+    uint32_t middle;
+    uint32_t minor;
+    uint32_t variety;
+    uint32_t internal;
+    char short_info[64];
+    char long_info[256];
+};
 
-void define_AppFileInfo(py::module_& m)
+PYBIND11_MODULE(AppFileInfo, m)
 {
 py::enum_<info_variety>(m, "info_variety", "")
 .value("B_DEVELOPMENT_VERSION", info_variety::B_DEVELOPMENT_VERSION, "")
@@ -29,7 +39,7 @@ py::enum_<version_kind>(m, "version_kind", "")
 .value("B_APP_VERSION_KIND", version_kind::B_APP_VERSION_KIND, "")
 .value("B_SYSTEM_VERSION_KIND", version_kind::B_SYSTEM_VERSION_KIND, "")
 .export_values();
-
+/*
 py::class_<version_info>(m, "version_info")
 .def_readwrite("major", &version_info::major, "")
 .def_readwrite("middle", &version_info::middle, "")
@@ -38,7 +48,16 @@ py::class_<version_info>(m, "version_info")
 .def_readwrite("internal", &version_info::internal, "")
 .def_readwrite("short_info", &version_info::short_info, "")
 .def_readwrite("long_info", &version_info::long_info, "")
-;
+;*/
+py::class_<version_info>(m, "version_info")
+.def(py::init<>())
+.def_readwrite("major", &version_info::major, "")
+.def_readwrite("middle", &version_info::middle, "")
+.def_readwrite("minor", &version_info::minor, "")
+.def_readwrite("variety", &version_info::variety, "")
+.def_readwrite("internal", &version_info::internal, "")
+.def_readwrite("short_info", &version_info::short_info, "")
+.def_readwrite("long_info", &version_info::long_info, "");
 
 py::class_<BAppFileInfo, BNodeInfo>(m, "BAppFileInfo")
 .def(py::init(), "")
@@ -59,20 +78,42 @@ py::class_<BAppFileInfo, BNodeInfo>(m, "BAppFileInfo")
 .def("SetSupportedTypes", py::overload_cast<const BMessage *>(&BAppFileInfo::SetSupportedTypes), "", py::arg("types"))
 .def("IsSupportedType", &BAppFileInfo::IsSupportedType, "", py::arg("type"))
 .def("Supports", &BAppFileInfo::Supports, "", py::arg("type"))
-.def("GetIcon", py::overload_cast<BBitmap *, icon_size>(&BAppFileInfo::GetIcon), "", py::arg("icon"), py::arg("which"))
+.def("GetIcon", py::overload_cast<BBitmap *, icon_size>(&BAppFileInfo::GetIcon, py::const_), "", py::arg("icon"), py::arg("which"))
 .def("SetIcon", py::overload_cast<const BBitmap *, icon_size, bool>(&BAppFileInfo::SetIcon), "", py::arg("icon"), py::arg("which"), py::arg("updateMimeDB"))
 .def("SetIcon", py::overload_cast<const BBitmap *, icon_size>(&BAppFileInfo::SetIcon), "", py::arg("icon"), py::arg("which"))
-.def("GetIcon", py::overload_cast<unsigned char, size_t *>(&BAppFileInfo::GetIcon), "", py::arg("data"), py::arg("size"))
-.def("SetIcon", py::overload_cast<unsigned char, size_t, bool>(&BAppFileInfo::SetIcon), "", py::arg("data"), py::arg("size"), py::arg("updateMimeDB"))
-.def("SetIcon", py::overload_cast<unsigned char, size_t>(&BAppFileInfo::SetIcon), "", py::arg("data"), py::arg("size"))
+//.def("GetIcon", py::overload_cast<unsigned char, size_t *>(&BAppFileInfo::GetIcon), "", py::arg("data"), py::arg("size"))
+.def("GetIcon", [](const BAppFileInfo& self, py::list& _data, size_t& _size) {
+            uint8* data = nullptr;
+            size_t size = 0;
+            status_t result = self.GetIcon(&data, &size);
+            for (size_t i = 0; i < size; ++i) {
+                _data.append(data[i]);
+            }
+            _size = size;
+            delete[] data;
+            return result;
+},"", py::arg("_data"), py::arg("_size"))
+//.def("SetIcon", py::overload_cast<unsigned char, size_t, bool>(&BAppFileInfo::SetIcon), "", py::arg("data"), py::arg("size"), py::arg("updateMimeDB")) //TODO
+//.def("SetIcon", py::overload_cast<unsigned char, size_t>(&BAppFileInfo::SetIcon), "", py::arg("data"), py::arg("size"))  //TODO
 .def("GetVersionInfo", &BAppFileInfo::GetVersionInfo, "", py::arg("info"), py::arg("kind"))
 .def("SetVersionInfo", &BAppFileInfo::SetVersionInfo, "", py::arg("info"), py::arg("kind"))
-.def("GetIconForType", py::overload_cast<const char *, BBitmap *, icon_size>(&BAppFileInfo::GetIconForType), "", py::arg("type"), py::arg("icon"), py::arg("which"))
-.def("GetIconForType", py::overload_cast<const char *, unsigned char, size_t *>(&BAppFileInfo::GetIconForType), "", py::arg("type"), py::arg("data"), py::arg("size"))
+.def("GetIconForType", py::overload_cast<const char *, BBitmap *, icon_size>(&BAppFileInfo::GetIconForType, py::const_), "", py::arg("type"), py::arg("icon"), py::arg("which"))
+//.def("GetIconForType", py::overload_cast<const char *, unsigned char, size_t *>(&BAppFileInfo::GetIconForType), "", py::arg("type"), py::arg("data"), py::arg("size"))
+.def("GetIconForType", [](const BAppFileInfo &self, const char *type, py::list& _data, size_t& _size) {
+            uint8* data = nullptr;
+            size_t size = 0;
+            status_t result = self.GetIconForType(type, &data, &size);
+            for (size_t i = 0; i < size; ++i) {
+                _data.append(data[i]);
+            }
+            _size = size;
+            delete[] data;
+            return result;
+},"", py::arg("type"), py::arg("_data"), py::arg("_size"))
 .def("SetIconForType", py::overload_cast<const char *, const BBitmap *, icon_size, bool>(&BAppFileInfo::SetIconForType), "", py::arg("type"), py::arg("icon"), py::arg("which"), py::arg("updateMimeDB"))
 .def("SetIconForType", py::overload_cast<const char *, const BBitmap *, icon_size>(&BAppFileInfo::SetIconForType), "", py::arg("type"), py::arg("icon"), py::arg("which"))
-.def("SetIconForType", py::overload_cast<const char *, unsigned char, size_t, bool>(&BAppFileInfo::SetIconForType), "", py::arg("type"), py::arg("data"), py::arg("size"), py::arg("updateMimeDB"))
-.def("SetIconForType", py::overload_cast<const char *, unsigned char, size_t>(&BAppFileInfo::SetIconForType), "", py::arg("type"), py::arg("data"), py::arg("size"))
+//.def("SetIconForType", py::overload_cast<const char *, unsigned char, size_t, bool>(&BAppFileInfo::SetIconForType), "", py::arg("type"), py::arg("data"), py::arg("size"), py::arg("updateMimeDB")) //TODO
+//.def("SetIconForType", py::overload_cast<const char *, unsigned char, size_t>(&BAppFileInfo::SetIconForType), "", py::arg("type"), py::arg("data"), py::arg("size")) //TODO
 .def("SetInfoLocation", &BAppFileInfo::SetInfoLocation, "", py::arg("location"))
 .def("IsUsingAttributes", &BAppFileInfo::IsUsingAttributes, "")
 .def("IsUsingResources", &BAppFileInfo::IsUsingResources, "")
