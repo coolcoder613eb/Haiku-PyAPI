d78aee63d0baf63a3f0056a3dc40013e8a01e944
Fabio Tomat <f.t.public@gmail.com>
2023-12-13 19:15:45 +0100

    Add ChannelSlider.cpp overrides

--- bindings/interface/ChannelSlider.cpp
+++ bindings/interface/ChannelSlider.cpp
@@ -9,6 +9,102 @@
 
 namespace py = pybind11;
 
+class PyBChannelSlider : public BChannelSlider{
+	public:
+        using BChannelSlider::BChannelSlider;
+        status_t	Archive(BMessage* into, bool deep = true) const override {
+        	PYBIND11_OVERLOAD(status_t, BChannelSlider, Archive, into, deep);
+        }
+        void		AttachedToWindow() override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, AttachedToWindow);
+        }
+        void		AllAttached() override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, AllAttached);
+        }
+        void		DetachedFromWindow() override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, DetachedFromWindow);
+        }
+        void		AllDetached() override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, AllDetached);
+        }
+        void		MessageReceived(BMessage* message) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, MessageReceived, message);
+        }
+        void				Draw(BRect updateRect) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, Draw, updateRect);
+        }
+        void				MouseDown(BPoint where) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, MouseDown, where);
+        }
+        void				MouseUp(BPoint where) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, MouseUp, where);
+        }
+        void				MouseMoved(BPoint where, uint32 transit, const BMessage* dragMessage) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, MouseMoved, where, transit, dragMessage);
+        }
+        void				WindowActivated(bool state) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, WindowActivated, state);
+        }
+        void				KeyDown(const char* bytes, int32 numBytes) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, KeyDown, bytes, numBytes);
+        }
+        void				KeyUp(const char* bytes, int32 numBytes) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, KeyUp, bytes, numBytes);
+        }
+        void				FrameResized(float width, float height) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, FrameResized, width, height);
+        }
+        void				SetFont(const BFont* font, uint32 mask = B_FONT_ALL) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, SetFont, font, mask);
+        }
+        void				MakeFocus(bool focusState = true) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, MakeFocus, focusState);
+        }
+        void				GetPreferredSize(float* _width, float* _height) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, GetPreferredSize, _width, _height);
+        }
+        BHandler*			ResolveSpecifier(BMessage* message, int32 index, BMessage* specifier, int32 form, const char* property) override {
+        	PYBIND11_OVERLOAD(BHandler*, BChannelSlider, ResolveSpecifier, message, index, specifier, form, property);
+        }
+        status_t			GetSupportedSuites(BMessage* data) override {
+        	PYBIND11_OVERLOAD(status_t, BChannelSlider, GetSupportedSuites, data);
+        }
+        void				SetEnabled(bool on) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, SetEnabled, on);
+        }
+        orientation			Orientation() const override {
+        	PYBIND11_OVERLOAD(orientation, BChannelSlider, Orientation);
+        }
+        int32				MaxChannelCount() const override {
+        	PYBIND11_OVERLOAD(int32, BChannelSlider, MaxChannelCount);
+        }
+        bool				SupportsIndividualLimits() const override {
+        	PYBIND11_OVERLOAD(bool, BChannelSlider, SupportsIndividualLimits);
+        }
+        void				DrawChannel(BView* into, int32 channel, BRect area, bool pressed) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, DrawChannel, into, channel, area, pressed);
+        }
+        void				DrawGroove(BView* into, int32 channel, BPoint leftTop, BPoint rightBottom) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, DrawGroove, into, channel, leftTop, rightBottom);
+        }
+        void				DrawThumb(BView* into, int32 channel, BPoint where, bool pressed) override {
+        	PYBIND11_OVERLOAD(void, BChannelSlider, DrawThumb, into, channel, where, pressed);
+        }
+        const BBitmap*		ThumbFor(int32 channel, bool pressed) override {
+        	PYBIND11_OVERLOAD(const BBitmap*, BChannelSlider, ThumbFor, channel, pressed);
+        }
+        BRect				ThumbFrameFor(int32 channel) override {
+        	PYBIND11_OVERLOAD(BRect, BChannelSlider, ThumbFrameFor, channel);
+        }
+        float				ThumbDeltaFor(int32 channel) override {
+        	PYBIND11_OVERLOAD(float, BChannelSlider, ThumbDeltaFor, channel);
+        }
+        float				ThumbRangeFor(int32 channel) override {
+        	PYBIND11_OVERLOAD(float, BChannelSlider, ThumbRangeFor, channel);
+        }
+};
+
+
 
 PYBIND11_MODULE(ChannelSlider,m)
 {
