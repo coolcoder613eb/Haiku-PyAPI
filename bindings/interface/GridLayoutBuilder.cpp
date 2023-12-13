8941e6d51d913ea2f9659b255366afa8c7155183
Fabio Tomat <f.t.public@gmail.com>
2023-12-13 15:34:39 +0100

    Fix 32bit

--- bindings/interface/GridLayoutBuilder.cpp
+++ bindings/interface/GridLayoutBuilder.cpp
@@ -20,8 +20,8 @@ py::class_<BGridLayoutBuilder>(m, "BGridLayoutBuilder")
 //.def("GetGridLayout", &BGridLayoutBuilder::GetGridLayout, "", py::arg("_layout")) //TODO
 .def("View", &BGridLayoutBuilder::View, "")
 //.def("GetView", &BGridLayoutBuilder::GetView, "", py::arg("_view")) // TODO
-.def("Add", py::overload_cast<BView *, int, int, int, int>(&BGridLayoutBuilder::Add), "", py::arg("view"), py::arg("column"), py::arg("row"), py::arg("columnCount")=1, py::arg("rowCount")=1)
-.def("Add", py::overload_cast<BLayoutItem *, int, int, int, int>(&BGridLayoutBuilder::Add), "", py::arg("item"), py::arg("column"), py::arg("row"), py::arg("columnCount")=1, py::arg("rowCount")=1)
+.def("Add", py::overload_cast<BView *, int32, int32, int32, int32>(&BGridLayoutBuilder::Add), "", py::arg("view"), py::arg("column"), py::arg("row"), py::arg("columnCount")=1, py::arg("rowCount")=1)
+.def("Add", py::overload_cast<BLayoutItem *, int32, int32, int32, int32>(&BGridLayoutBuilder::Add), "", py::arg("item"), py::arg("column"), py::arg("row"), py::arg("columnCount")=1, py::arg("rowCount")=1)
 .def("SetColumnWeight", &BGridLayoutBuilder::SetColumnWeight, "", py::arg("column"), py::arg("weight"))
 .def("SetRowWeight", &BGridLayoutBuilder::SetRowWeight, "", py::arg("row"), py::arg("weight"))
 .def("SetInsets", &BGridLayoutBuilder::SetInsets, "", py::arg("left"), py::arg("top"), py::arg("right"), py::arg("bottom"))
