6ecd8152d8aad6c78ac18a511adf86afe21dfef4
Fabio Tomat <f.t.public@gmail.com>
2023-12-18 15:31:51 +0100

    Initial TranslationUtils and TranslatorRoster

--- bindings/translation/TranslationUtils.cpp
+++ bindings/translation/TranslationUtils.cpp
@@ -4,16 +4,24 @@
 #include <pybind11/operators.h>
 
 #include <translation/TranslationUtils.h>
+#include <TranslatorRoster.h>
+#include <Bitmap.h>
+#include <Entry.h>
+#include <DataIO.h>
+#include <TextView.h>
+#include <File.h>
+#include <Message.h>
+#include <Menu.h>
 
 namespace py = pybind11;
 
 
-void define_TranslationUtils(py::module_& m)
+PYBIND11_MODULE(TranslationUtils, m)
 {
-py::class_<BTranslationUtils>(m, "BTranslationUtils")
+py::class_<BTranslationUtils,std::unique_ptr<BTranslationUtils, py::nodelete>>(m, "BTranslationUtils")
 .def_static("GetBitmap", py::overload_cast<const char *, BTranslatorRoster *>(&BTranslationUtils::GetBitmap), "", py::arg("name"), py::arg("roster")=NULL)
-.def_static("GetBitmap", py::overload_cast<unsigned int, int, BTranslatorRoster *>(&BTranslationUtils::GetBitmap), "", py::arg("type"), py::arg("id"), py::arg("roster")=NULL)
-.def_static("GetBitmap", py::overload_cast<unsigned int, const char *, BTranslatorRoster *>(&BTranslationUtils::GetBitmap), "", py::arg("type"), py::arg("name"), py::arg("roster")=NULL)
+.def_static("GetBitmap", py::overload_cast<uint32, int32, BTranslatorRoster *>(&BTranslationUtils::GetBitmap), "", py::arg("type"), py::arg("id"), py::arg("roster")=NULL)
+.def_static("GetBitmap", py::overload_cast<uint32, const char *, BTranslatorRoster *>(&BTranslationUtils::GetBitmap), "", py::arg("type"), py::arg("name"), py::arg("roster")=NULL)
 .def_static("GetBitmapFile", &BTranslationUtils::GetBitmapFile, "", py::arg("name"), py::arg("roster")=NULL)
 .def_static("GetBitmap", py::overload_cast<const entry_ref *, BTranslatorRoster *>(&BTranslationUtils::GetBitmap), "", py::arg("ref"), py::arg("roster")=NULL)
 .def_static("GetBitmap", py::overload_cast<BPositionIO *, BTranslatorRoster *>(&BTranslationUtils::GetBitmap), "", py::arg("stream"), py::arg("roster")=NULL)
@@ -25,7 +33,7 @@ py::class_<BTranslationUtils>(m, "BTranslationUtils")
 .def_static("WriteStyledEditFile", py::overload_cast<BTextView *, BFile *>(&BTranslationUtils::WriteStyledEditFile), "", py::arg("fromView"), py::arg("intoFile"))
 .def_static("WriteStyledEditFile", py::overload_cast<BTextView *, BFile *, const char *>(&BTranslationUtils::WriteStyledEditFile), "", py::arg("fromView"), py::arg("intoFile"), py::arg("encoding"))
 .def_static("GetDefaultSettings", py::overload_cast<translator_id, BTranslatorRoster *>(&BTranslationUtils::GetDefaultSettings), "", py::arg("translator"), py::arg("roster")=NULL)
-.def_static("GetDefaultSettings", py::overload_cast<const char *, int>(&BTranslationUtils::GetDefaultSettings), "", py::arg("name"), py::arg("version"))
+.def_static("GetDefaultSettings", py::overload_cast<const char *, int32>(&BTranslationUtils::GetDefaultSettings), "", py::arg("name"), py::arg("version"))
 .def_static("AddTranslationItems", &BTranslationUtils::AddTranslationItems, "", py::arg("intoMenu"), py::arg("fromType"), py::arg("model")=NULL, py::arg("idName")=NULL, py::arg("typeName")=NULL, py::arg("roster")=NULL)
 ;
 
--- bindings/translation/TranslatorRoster.cpp
+++ bindings/translation/TranslatorRoster.cpp
@@ -4,11 +4,14 @@
 #include <pybind11/operators.h>
 
 #include <translation/TranslatorRoster.h>
+#include <Translator.h>
+#include <Messenger.h>
+#include <View.h>
 
 namespace py = pybind11;
 
 
-void define_TranslatorRoster(py::module_& m)
+PYBIND11_MODULE(TranslatorRoster, m)
 {
 py::class_<BTranslatorReleaseDelegate>(m, "BTranslatorReleaseDelegate")
 .def(py::init<BTranslator *>(), "", py::arg("translator"))
@@ -24,21 +27,21 @@ py::class_<BTranslatorRoster, BArchivable>(m, "BTranslatorRoster")
 .def("AddTranslators", &BTranslatorRoster::AddTranslators, "", py::arg("loadPath")=NULL)
 .def("AddTranslator", &BTranslatorRoster::AddTranslator, "", py::arg("translator"))
 .def("Identify", &BTranslatorRoster::Identify, "", py::arg("source"), py::arg("ioExtension"), py::arg("_info"), py::arg("hintType")=0, py::arg("hintMIME")=NULL, py::arg("wantType")=0)
-.def("GetTranslators", &BTranslatorRoster::GetTranslators, "", py::arg("source"), py::arg("ioExtension"), py::arg("_info"), py::arg("_numInfo"), py::arg("hintType")=0, py::arg("hintMIME")=NULL, py::arg("wantType")=0)
-.def("GetAllTranslators", &BTranslatorRoster::GetAllTranslators, "", py::arg("_list"), py::arg("_count"))
-.def("GetTranslatorInfo", &BTranslatorRoster::GetTranslatorInfo, "", py::arg("translatorID"), py::arg("_name"), py::arg("_info"), py::arg("_version"))
-.def("GetInputFormats", &BTranslatorRoster::GetInputFormats, "", py::arg("translatorID"), py::arg("_formats"), py::arg("_numFormats"))
-.def("GetOutputFormats", &BTranslatorRoster::GetOutputFormats, "", py::arg("translatorID"), py::arg("_formats"), py::arg("_numFormats"))
-.def("Translate", py::overload_cast<BPositionIO *, const translator_info *, BMessage *, BPositionIO *, unsigned int, unsigned int, const char *>(&BTranslatorRoster::Translate), "", py::arg("source"), py::arg("info"), py::arg("ioExtension"), py::arg("destination"), py::arg("wantOutType"), py::arg("hintType")=0, py::arg("hintMIME")=NULL)
-.def("Translate", py::overload_cast<translator_id, BPositionIO *, BMessage *, BPositionIO *, unsigned int>(&BTranslatorRoster::Translate), "", py::arg("translatorID"), py::arg("source"), py::arg("ioExtension"), py::arg("destination"), py::arg("wantOutType"))
-.def("MakeConfigurationView", &BTranslatorRoster::MakeConfigurationView, "", py::arg("translatorID"), py::arg("ioExtension"), py::arg("_view"), py::arg("_extent"))
+//.def("GetTranslators", &BTranslatorRoster::GetTranslators, "", py::arg("source"), py::arg("ioExtension"), py::arg("_info"), py::arg("_numInfo"), py::arg("hintType")=0, py::arg("hintMIME")=NULL, py::arg("wantType")=0)
+//.def("GetAllTranslators", &BTranslatorRoster::GetAllTranslators, "", py::arg("_list"), py::arg("_count"))
+//.def("GetTranslatorInfo", &BTranslatorRoster::GetTranslatorInfo, "", py::arg("translatorID"), py::arg("_name"), py::arg("_info"), py::arg("_version"))
+//.def("GetInputFormats", &BTranslatorRoster::GetInputFormats, "", py::arg("translatorID"), py::arg("_formats"), py::arg("_numFormats"))
+//.def("GetOutputFormats", &BTranslatorRoster::GetOutputFormats, "", py::arg("translatorID"), py::arg("_formats"), py::arg("_numFormats"))
+.def("Translate", py::overload_cast<BPositionIO *, const translator_info *, BMessage *, BPositionIO *, uint32, uint32, const char *>(&BTranslatorRoster::Translate), "", py::arg("source"), py::arg("info"), py::arg("ioExtension"), py::arg("destination"), py::arg("wantOutType"), py::arg("hintType")=0, py::arg("hintMIME")=NULL)
+.def("Translate", py::overload_cast<translator_id, BPositionIO *, BMessage *, BPositionIO *, uint32>(&BTranslatorRoster::Translate), "", py::arg("translatorID"), py::arg("source"), py::arg("ioExtension"), py::arg("destination"), py::arg("wantOutType"))
+//.def("MakeConfigurationView", &BTranslatorRoster::MakeConfigurationView, "", py::arg("translatorID"), py::arg("ioExtension"), py::arg("_view"), py::arg("_extent"))
 .def("GetConfigurationMessage", &BTranslatorRoster::GetConfigurationMessage, "", py::arg("translatorID"), py::arg("ioExtension"))
 .def("AcquireTranslator", &BTranslatorRoster::AcquireTranslator, "", py::arg("translatorID"))
 .def("GetRefFor", &BTranslatorRoster::GetRefFor, "", py::arg("translatorID"), py::arg("ref"))
 .def("IsTranslator", &BTranslatorRoster::IsTranslator, "", py::arg("ref"))
 .def("StartWatching", &BTranslatorRoster::StartWatching, "", py::arg("target"))
 .def("StopWatching", &BTranslatorRoster::StopWatching, "", py::arg("target"))
-.def_readwrite("Private", &BTranslatorRoster::Private, "")
+//.def_readwrite("Private", &BTranslatorRoster::Private, "")
 ;
 
 
