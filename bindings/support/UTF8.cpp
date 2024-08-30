4e750481ef6807c7dba8b00723ce0ba3ddc2744c
Fabio Tomat <f.t.public@gmail.com>
2024-08-30 18:53:33 +0200

    add UTF8 support

--- bindings/support/UTF8.cpp
+++ bindings/support/UTF8.cpp
@@ -4,43 +4,44 @@
 #include <pybind11/operators.h>
 
 #include <support/UTF8.h>
+#include <InterfaceDefs.h>
 
 namespace py = pybind11;
 
 
-void define_UTF8(py::module_& m)
+PYBIND11_MODULE(UTF8, m)
 {
-m.attr("B_ISO1_CONVERSION") = py::cast(B_ISO1_CONVERSION);
-m.attr("B_ISO2_CONVERSION") = py::cast(B_ISO2_CONVERSION);
-m.attr("B_ISO3_CONVERSION") = py::cast(B_ISO3_CONVERSION);
-m.attr("B_ISO4_CONVERSION") = py::cast(B_ISO4_CONVERSION);
-m.attr("B_ISO5_CONVERSION") = py::cast(B_ISO5_CONVERSION);
-m.attr("B_ISO6_CONVERSION") = py::cast(B_ISO6_CONVERSION);
-m.attr("B_ISO7_CONVERSION") = py::cast(B_ISO7_CONVERSION);
-m.attr("B_ISO8_CONVERSION") = py::cast(B_ISO8_CONVERSION);
-m.attr("B_ISO9_CONVERSION") = py::cast(B_ISO9_CONVERSION);
-m.attr("B_ISO10_CONVERSION") = py::cast(B_ISO10_CONVERSION);
-m.attr("B_MAC_ROMAN_CONVERSION") = py::cast(B_MAC_ROMAN_CONVERSION);
-m.attr("B_SJIS_CONVERSION") = py::cast(B_SJIS_CONVERSION);
-m.attr("B_EUC_CONVERSION") = py::cast(B_EUC_CONVERSION);
-m.attr("B_JIS_CONVERSION") = py::cast(B_JIS_CONVERSION);
-m.attr("B_MS_WINDOWS_CONVERSION") = py::cast(B_MS_WINDOWS_CONVERSION);
-m.attr("B_UNICODE_CONVERSION") = py::cast(B_UNICODE_CONVERSION);
-m.attr("B_KOI8R_CONVERSION") = py::cast(B_KOI8R_CONVERSION);
-m.attr("B_MS_WINDOWS_1251_CONVERSION") = py::cast(B_MS_WINDOWS_1251_CONVERSION);
-m.attr("B_MS_DOS_866_CONVERSION") = py::cast(B_MS_DOS_866_CONVERSION);
-m.attr("B_MS_DOS_CONVERSION") = py::cast(B_MS_DOS_CONVERSION);
-m.attr("B_EUC_KR_CONVERSION") = py::cast(B_EUC_KR_CONVERSION);
-m.attr("B_ISO13_CONVERSION") = py::cast(B_ISO13_CONVERSION);
-m.attr("B_ISO14_CONVERSION") = py::cast(B_ISO14_CONVERSION);
-m.attr("B_ISO15_CONVERSION") = py::cast(B_ISO15_CONVERSION);
-m.attr("B_BIG5_CONVERSION") = py::cast(B_BIG5_CONVERSION);
-m.attr("B_GBK_CONVERSION") = py::cast(B_GBK_CONVERSION);
-m.attr("B_UTF16_CONVERSION") = py::cast(B_UTF16_CONVERSION);
-m.attr("B_MS_WINDOWS_1250_CONVERSION") = py::cast(B_MS_WINDOWS_1250_CONVERSION);
+m.attr("B_ISO1_CONVERSION") = 0;//py::cast(B_ISO....);
+m.attr("B_ISO2_CONVERSION") = 1;
+m.attr("B_ISO3_CONVERSION") = 2;
+m.attr("B_ISO4_CONVERSION") = 3;
+m.attr("B_ISO5_CONVERSION") = 4;
+m.attr("B_ISO6_CONVERSION") = 5;
+m.attr("B_ISO7_CONVERSION") = 6;
+m.attr("B_ISO8_CONVERSION") = 7;
+m.attr("B_ISO9_CONVERSION") = 8;
+m.attr("B_ISO10_CONVERSION") = 9;
+m.attr("B_MAC_ROMAN_CONVERSION") = 10;
+m.attr("B_SJIS_CONVERSION") = 11;
+m.attr("B_EUC_CONVERSION") = 12;
+m.attr("B_JIS_CONVERSION") = 13;
+m.attr("B_MS_WINDOWS_CONVERSION") = 14;
+m.attr("B_UNICODE_CONVERSION") = 15;
+m.attr("B_KOI8R_CONVERSION") = 16;
+m.attr("B_MS_WINDOWS_1251_CONVERSION") = 17;
+m.attr("B_MS_DOS_866_CONVERSION") = 18;
+m.attr("B_MS_DOS_CONVERSION") = 19;
+m.attr("B_EUC_KR_CONVERSION") = 20;
+m.attr("B_ISO13_CONVERSION") = 21;
+m.attr("B_ISO14_CONVERSION") = 22;
+m.attr("B_ISO15_CONVERSION") = 23;
+m.attr("B_BIG5_CONVERSION") = 24;
+m.attr("B_GBK_CONVERSION") = 25;
+m.attr("B_UTF16_CONVERSION") = 26;
+m.attr("B_MS_WINDOWS_1250_CONVERSION") = 27;
 
-m.def("convert_to_utf8", &convert_to_utf8, "", py::arg("sourceEncoding"), py::arg("source"), py::arg("sourceLength"), py::arg("dest"), py::arg("destLength"), py::arg("state"), py::arg("substitute")=B_SUBSTITUTE);
+m.def("convert_to_utf8", &convert_to_utf8, "", py::arg("sourceEncoding"), py::arg("source"), py::arg("sourceLength"), py::arg("dest"), py::arg("destLength"), py::arg("state"), py::arg("substitute")=0x1a);//hardcoded B_SUBSTITUTE
 
-m.def("convert_from_utf8", &convert_from_utf8, "", py::arg("destEncoding"), py::arg("source"), py::arg("sourceLength"), py::arg("dest"), py::arg("destLength"), py::arg("state"), py::arg("substitute")=B_SUBSTITUTE);
+m.def("convert_from_utf8", &convert_from_utf8, "", py::arg("destEncoding"), py::arg("source"), py::arg("sourceLength"), py::arg("dest"), py::arg("destLength"), py::arg("state"), py::arg("substitute")=0x1a);//hardcoded B_SUBSTITUTE
 
 }
