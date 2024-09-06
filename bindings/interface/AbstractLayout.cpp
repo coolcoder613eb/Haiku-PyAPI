ea781217648b2f8519f94a2c4d4cd9940c8889d0
Fabio Tomat <f.t.public@gmail.com>
2024-09-06 16:15:38 +0200

    update AbstractLayout TwoDimensionalLayout and overrides abstract-classes

--- bindings/interface/AbstractLayout.cpp
+++ bindings/interface/AbstractLayout.cpp
@@ -67,9 +67,32 @@ class PyBAbstractLayout : public BAbstractLayout{
         status_t		Perform(perform_code d, void* arg) override {
         	PYBIND11_OVERLOAD(status_t, BAbstractLayout, Perform, d, arg);
         }
+        void            DoLayout() override {
+        	PYBIND11_OVERLOAD_PURE(void, BAbstractLayout, DoLayout);
+        }
 };
 
 PYBIND11_MODULE(AbstractLayout,m)
 {
-py::class_<BAbstractLayout,PyBAbstractLayout, BLayout>(m, "BAbstractLayout");
+py::class_<BAbstractLayout,PyBAbstractLayout, BLayout>(m, "BAbstractLayout")
+.def(py::init(), "")
+.def(py::init<BMessage *>(), "", py::arg("from"))
+.def("MinSize", &BAbstractLayout::MinSize, "")
+.def("MaxSize", &BAbstractLayout::MaxSize, "")
+.def("PreferredSize", &BAbstractLayout::PreferredSize, "")
+.def("Alignment", &BAbstractLayout::Alignment, "")
+.def("SetExplicitMinSize", &BAbstractLayout::SetExplicitMinSize, "", py::arg("size"))
+.def("SetExplicitMaxSize", &BAbstractLayout::SetExplicitMaxSize, "", py::arg("size"))
+.def("SetExplicitPreferredSize", &BAbstractLayout::SetExplicitPreferredSize, "", py::arg("size"))
+.def("SetExplicitAlignment", &BAbstractLayout::SetExplicitAlignment, "", py::arg("alignment"))
+.def("BaseMinSize", &BAbstractLayout::BaseMinSize, "")
+.def("BaseMaxSize", &BAbstractLayout::BaseMaxSize, "")
+.def("BasePreferredSize", &BAbstractLayout::BasePreferredSize, "")
+.def("BaseAlignment", &BAbstractLayout::BaseAlignment, "")
+.def("Frame", &BAbstractLayout::Frame, "")
+.def("SetFrame", &BAbstractLayout::SetFrame, "", py::arg("frame"))
+.def("IsVisible", &BAbstractLayout::IsVisible, "")
+.def("SetVisible", &BAbstractLayout::SetVisible, "", py::arg("visible"))
+.def("Archive", &BAbstractLayout::Archive, "", py::arg("into"), py::arg("deep")=true)
+.def("Perform", &BAbstractLayout::Perform, "", py::arg("d"), py::arg("arg"));
 }
--- bindings/interface/TwoDimensionalLayout.cpp
+++ bindings/interface/TwoDimensionalLayout.cpp
@@ -8,8 +8,68 @@
 
 namespace py = pybind11;
 
+class PyBTwoDimensionalLayout : public BTwoDimensionalLayout{
+	public:
+        using BTwoDimensionalLayout::BTwoDimensionalLayout;
+        BSize				BaseMinSize() override {
+        	PYBIND11_OVERLOAD(BSize, BTwoDimensionalLayout, BaseMinSize);
+        }
+		BSize				BaseMaxSize() override {
+        	PYBIND11_OVERLOAD(BSize, BTwoDimensionalLayout, BaseMaxSize);
+        }
+		BSize				BasePreferredSize() override {
+        	PYBIND11_OVERLOAD(BSize, BTwoDimensionalLayout, BasePreferredSize);
+        }
+		BAlignment			BaseAlignment() override {
+        	PYBIND11_OVERLOAD(BAlignment, BTwoDimensionalLayout, BaseAlignment);
+        }
+		bool				HasHeightForWidth() override {
+        	PYBIND11_OVERLOAD(bool, BTwoDimensionalLayout, HasHeightForWidth);
+        }
+		void				GetHeightForWidth(float width, float* min, float* max, float* preferred) override {
+        	PYBIND11_OVERLOAD(void, BTwoDimensionalLayout, GetHeightForWidth, width, min, max, preferred);
+        }
+		void				SetFrame(BRect frame) override {
+        	PYBIND11_OVERLOAD(void, BTwoDimensionalLayout, SetFrame, frame);
+        }
+		status_t			Archive(BMessage* into, bool deep = true) const override {
+        	PYBIND11_OVERLOAD(status_t, BTwoDimensionalLayout, Archive, into, deep);
+        }
+		status_t			Perform(perform_code d, void* arg) override {
+        	PYBIND11_OVERLOAD(status_t, BTwoDimensionalLayout, Perform, d, arg);
+        }
+        int32				InternalCountColumns() override {
+        	PYBIND11_OVERLOAD_PURE(int32, BTwoDimensionalLayout, InternalCountColumns);
+        }
+        int32				InternalCountRows() override {
+        	PYBIND11_OVERLOAD_PURE(int32, BTwoDimensionalLayout, InternalCountRows);
+        }
+        void				GetColumnRowConstraints(orientation orientation, int32 index, ColumnRowConstraints* constraints) override {
+        	PYBIND11_OVERLOAD_PURE(void, BTwoDimensionalLayout, GetColumnRowConstraints, orientation, index, constraints);
+        }
+		void				GetItemDimensions(BLayoutItem* item, Dimensions* dimensions) override {
+        	PYBIND11_OVERLOAD_PURE(void, BTwoDimensionalLayout, GetItemDimensions, item, dimensions);
+        }
+};
 
 PYBIND11_MODULE(TwoDimensionalLayout,m)
 {
-py::class_<BTwoDimensionalLayout, BAbstractLayout>(m, "BTwoDimensionalLayout");
+py::class_<BTwoDimensionalLayout,PyBTwoDimensionalLayout, BAbstractLayout>(m, "BTwoDimensionalLayout")
+.def(py::init(), "")
+.def(py::init<BMessage *>(), "", py::arg("from"))
+.def("SetInsets", py::overload_cast<float, float, float, float>(&BTwoDimensionalLayout::SetInsets), "", py::arg("left"), py::arg("top"), py::arg("right"), py::arg("bottom"))
+.def("SetInsets", py::overload_cast<float, float>(&BTwoDimensionalLayout::SetInsets), "", py::arg("horizontal"), py::arg("vertical"))
+.def("SetInsets", py::overload_cast<float>(&BTwoDimensionalLayout::SetInsets), "", py::arg("insets"))
+.def("GetInsets", &BTwoDimensionalLayout::GetInsets, "", py::arg("left"), py::arg("top"), py::arg("right"), py::arg("bottom"))
+.def("AlignLayoutWith", &BTwoDimensionalLayout::AlignLayoutWith, "", py::arg("other"), py::arg("orientation"))
+.def("BaseMinSize", &BTwoDimensionalLayout::BaseMinSize, "")
+.def("BaseMaxSize", &BTwoDimensionalLayout::BaseMaxSize, "")
+.def("BasePreferredSize", &BTwoDimensionalLayout::BasePreferredSize, "")
+.def("BaseAlignment", &BTwoDimensionalLayout::BaseAlignment, "")
+.def("HasHeightForWidth", &BTwoDimensionalLayout::HasHeightForWidth, "")
+.def("GetHeightForWidth", &BTwoDimensionalLayout::GetHeightForWidth, "", py::arg("width"), py::arg("min"), py::arg("max"), py::arg("preferred"))
+.def("SetFrame", &BTwoDimensionalLayout::SetFrame, "", py::arg("frame"))
+.def("Archive", &BTwoDimensionalLayout::Archive, "", py::arg("into"), py::arg("deep")=true)
+.def("Perform", &BTwoDimensionalLayout::Perform, "", py::arg("d"), py::arg("arg"))
+;
 }
