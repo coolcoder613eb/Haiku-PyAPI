f0f01ae48c7adda003ae0a68c42ac59e0b2487e8
Fabio Tomat <f.t.public@gmail.com>
2024-09-09 15:24:49 +0200

    test purpose,revert please

--- bindings/support/BlockCache.cpp
+++ bindings/support/BlockCache.cpp
@@ -36,7 +36,32 @@ py::class_<BBlockCache>(m, "BBlockCache")
 .def(py::init<uint32, size_t, uint32>(), "", py::arg("blockCount"), py::arg("blockSize"), py::arg("allocationType"))
 //.def("Get", &BBlockCache::Get, "", py::arg("blockSize"))//TODO this returns a void * -- let's return a py::bytes or a py::buffer
 .def("Get", &Get_wrapper2, "", py::arg("blockSize"))
-.def("Save", &BBlockCache::Save, "", py::arg("pointer"), py::arg("blockSize"))//TODO: pointer is a void * -> python can't use void *
+.def("Get2", [](BBlockCache& self, size_t blockSize)->py::tuple{
+	std::vector<char>buffer(blockSize);
+	void* data = self.Get(blockSize);
+	if (data != nullptr) {
+	std::memcpy(buffer.data(), data, blockSize);
+	}
+	py::bytes pyBytes(buffer.data(),blockSize);
+	return py::make_tuple(pyBytes, blockSize);
+},"",py::arg("blockSize"))
+//.def("Save", &BBlockCache::Save, "", py::arg("pointer"), py::arg("blockSize"))//TODO: pointer is a void * -> python can't use void *
+.def("Save", [](BBlockCache& self, py::bytes data) {//if we pass a py::bytes we already pass the size along with data right?
+/*	std::string byt = py::str(data);
+	void* pointer = static_cast<void*>(const_cast<char*>(byt.data()));
+	self.Save(pointer, byt.size());*/
+	char* pointer;
+	ssize_t size;
+	PYBIND11_BYTES_AS_STRING_AND_SIZE(data.ptr(), &pointer, &size);
+	self.Save(static_cast<void*>(pointer), static_cast<size_t>(size));
+	},"", py::arg("data"))
+.def("Save_from_buffer", [](BBlockCache& self, py::buffer py_buffer){
+	py::buffer_info buf_info = py_buffer.request();
+	void* pointer = buf_info.ptr;
+	//size_t blockSize = buf_info.size * buf_info.itemsize;
+	size_t blockSize = buf_info.size;
+	self.Save(pointer, blockSize);
+},"",py::arg("py_buffer"))
 ;
 
 
