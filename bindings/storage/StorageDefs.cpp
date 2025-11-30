6f849dcde15d224af2dbb07617870e859bbebceb
Fabio Tomat <f.t.public@gmail.com>
2025-11-30 12:45:47 +0100

    Add definitions in StorageDefs

--- bindings/storage/StorageDefs.cpp
+++ bindings/storage/StorageDefs.cpp
@@ -15,13 +15,30 @@ namespace py = pybind11;
 //using namespace BPackageKit;
 
 
+
 PYBIND11_MODULE(StorageDefs,m)
 {
+m.attr("B_DEV_NAME_LENGTH") = B_DEV_NAME_LENGTH; //128
+m.attr("B_FILE_NAME_LENGTH") = B_FILE_NAME_LENGTH; //NAME_MAX
+m.attr("B_PATH_NAME_LENGTH") = B_PATH_NAME_LENGTH; //MAXPATHLEN
+m.attr("B_ATTR_NAME_LENGTH") = B_ATTR_NAME_LENGTH; //(B_FILE_NAME_LENGTH - 1)
+m.attr("B_MIME_TYPE_LENGTH") = B_MIME_TYPE_LENGTH; //(B_ATTR_NAME_LENGTH - 15)
+m.attr("B_MAX_SYMLINKS") = B_MAX_SYMLINKS; //SYMLOOP_MAX
+
+
 py::enum_<node_flavor>(m, "node_flavor", "")
 .value("B_FILE_NODE", node_flavor::B_FILE_NODE, "")
 .value("B_SYMLINK_NODE", node_flavor::B_SYMLINK_NODE, "")
 .value("B_DIRECTORY_NODE", node_flavor::B_DIRECTORY_NODE, "")
 .value("B_ANY_NODE", node_flavor::B_ANY_NODE, "")
 .export_values();
-
+m.attr("B_READ_ONLY") = 0;
+m.attr("B_WRITE_ONLY") = 1;
+m.attr("B_READ_WRITE") = 2;
+m.attr("B_FAIL_IF_EXISTS") = 4;
+m.attr("B_CREATE_FILE") = 8;
+m.attr("B_ERASE_FILE") = 64;
+m.attr("B_OPEN_AT_END") = 128;
+//m.attr("B_CLONE_KIND") = 1048576;
 }
+
